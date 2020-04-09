a6c7750bb8c4e559662e7e6f4d711a3e88f177fa
Cyril de Wit <cyrilwit@gmail.com>
2020-04-09 11:54:31 +0200

    feat: add Enrollment resource

---
+++ app/App/Course/Resources/Enrollment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Course\Resources;
+
+use App\User\Resources\UserCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Enrollment extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                     => $this->id,
+            'course_id'                   => $this->course_id,
+            'user_id'                  => $this->user_id,
+            'created_at'             => $this->created_at,
+            'updated_at'             => $this->updated_at,
+        ];
+    }
+}
